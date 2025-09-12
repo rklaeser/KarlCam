f14c708170415eb7fe495c551f3aeeb8429e677c
Reed Klaeser <reed.klaeser@gmail.com>
2025-09-12 10:37:59 -0700

    fix: port attempt to go back

--- web/api/main.py
+++ web/api/main.py
@@ -258,8 +258,9 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(
-        app,
+        "main:app",
         host="0.0.0.0",
         port=port,
-        log_level="debug" if settings.DEBUG else "info"
+        log_level="debug" if settings.DEBUG else "info",
+        reload=False
     )
